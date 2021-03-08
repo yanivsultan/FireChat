dba171da45a364d7efe3093a5e837056a7ff1330
Yaniv <yaniv@Yanivs-MacBook-Pro.local>
2021-03-08 12:39:29 +0200

    finished design v1

--- src/ChatMessage.js
+++ src/ChatMessage.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import './components/ChatMessage.css'
 
 const ChatMessage = ({message, auth}) => {
 
-    const messageClass = message.uid===auth.currentUser.uid ? 'sent' : 'recieved';
+    const messageClass = message.uid===auth.currentUser.uid ? 'Sent' : 'Recieved';
     return (
-        <div className={`message_${messageClass}`}>
+        <div className={`ChatMessage Message${messageClass}`}>
             <img src={message.photoURL} alt=""/>
+            <span className="Message">
             {message.text}
+            </span>
         </div>
     )
 }
